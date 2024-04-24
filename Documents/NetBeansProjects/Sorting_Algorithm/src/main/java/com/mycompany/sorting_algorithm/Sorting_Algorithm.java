56e2d465bbccd3b549f4e7b74b447fa2971ca80c
Clarissa B. Vega <issayvega@gmail.com>
2024-04-24 19:47:29 +0800

    java

---
+++ Documents/NetBeansProjects/Sorting_Algorithm/src/main/java/com/mycompany/sorting_algorithm/Sorting_Algorithm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sorting_algorithm;
+
+/**
+ *
+ * @author issay
+ */
+public class Sorting_Algorithm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
