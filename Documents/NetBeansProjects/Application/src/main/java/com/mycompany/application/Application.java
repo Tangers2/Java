7acd8a529e7177efadeeb62a46ccf42e0223cc43
Kru_kru <135120157+Tangers2@users.noreply.github.com>
2024-05-06 22:53:02 +0800

    java

---
+++ Documents/NetBeansProjects/Application/src/main/java/com/mycompany/application/Application.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.application;
+
+/**
+ *
+ * @author issay
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        ticTactoe tictactoe = new ticTactoe();
+        
+        
+    }
+}
